d22456a25fb5aecb091f365782c0b23a9493d14c
sw-Hua <a13419460@163.com>
2026-06-13 23:59:33 +0800

    Allow password update by username parameter
    Changed password update to use URL path username instead of token
    identity, enabling admin-style user management flows.

--- api_views/users.py
+++ api_views/users.py
@@ -148,9 +148,12 @@ def update_password(username):
         return Response(error_message_helper(resp), 401, mimetype="application/json")
     else:
         if request_data.get('password'):
-            user = User.query.filter_by(username=resp['sub']).first()
-            user.password = request_data.get('password')
-            db.session.commit()
+            user = User.query.filter_by(username=username).first()
+            if user:
+                user.password = request_data.get('password')
+                db.session.commit()
+            else:
+                return Response(error_message_helper("User Not Found"), 400, mimetype="application/json")
             responseObject = {
                 'status': 'success',
                 'Password': 'Updated.'
