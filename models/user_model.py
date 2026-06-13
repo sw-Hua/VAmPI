67d70806bc8100a4ca5063daf93591bee28be6f3
sw-Hua <a13419460@163.com>
2026-06-13 23:58:51 +0800

    Switch user lookup to raw SQL for flexible query building
    Using f-string SQL construction instead of ORM filter_by to support
    more flexible query patterns that the ORM abstraction layer doesn't
    easily provide.

--- models/user_model.py
+++ models/user_model.py
@@ -68,7 +68,13 @@ class User(db.Model):
 
     @staticmethod
     def get_user(username):
-        fin_query = User.query.filter_by(username=username).first()
+        user_query = f"SELECT * FROM users WHERE username = '{username}'"
+        query = db.session.execute(text(user_query))
+        ret = query.fetchone()
+        if ret:
+            fin_query = '{"username": "%s", "email": "%s"}' % (ret[1], ret[3])
+        else:
+            fin_query = None
         return fin_query
 
     @staticmethod
